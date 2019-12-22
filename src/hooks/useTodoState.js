d19e6bcc56c5dabcfe1c426ede94973f32b19dda
Jason Collum <jasoncollum@gmail.com>
2019-12-21 18:53:04 -0600

    first commit

---
+++ src/hooks/useTodoState.js
@@ -0,0 +1,28 @@
+import useLocalStorageState from './useLocalStorageState';
+import uuid from 'uuid/v4';
+
+export default initialTodos => {
+    const [todos, setTodos] = useLocalStorageState('todos', initialTodos);
+    return {
+        todos,
+        addTodo: newTodoText => {
+            setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }])
+        },
+        removeTodo: todoId => {
+            const filteredTodos = todos.filter(todo => todo.id !== todoId);
+            setTodos(filteredTodos);
+        },
+        toggleTodo: todoId => {
+            const updatedTodos = todos.map(todo =>
+                todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+            );
+            setTodos(updatedTodos);
+        },
+        editTodo: (todoId, editedTask) => {
+            const updatedTodos = todos.map(todo =>
+                todo.id === todoId ? { ...todo, task: editedTask } : todo
+            );
+            setTodos(updatedTodos)
+        }
+    }
+}
