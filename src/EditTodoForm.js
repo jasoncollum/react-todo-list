381b470cda406677c681690a680db90606a671d8
Jason Collum <jasoncollum@gmail.com>
2019-12-30 10:40:12 -0600

    refactor TodoForm, TodoList, Todo, and EditTodoForm to consume TodosContext

--- src/EditTodoForm.js
+++ src/EditTodoForm.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TodosContext } from './context/todos.context';
 import TextField from '@material-ui/core/TextField';
 import useInputState from './hooks/useInputState';
 
-function EditTodoForm({ id, task, toggleEditForm, editTodo }) {
+function EditTodoForm({ id, task, toggleEditForm }) {
+    const { editTodo } = useContext(TodosContext);
     const [value, handleChange, reset] = useInputState(task);
     return (
         <form
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import useTodoState from './hooks/useTodoState';
 import { TodosProvider } from './context/todos.context';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
@@ -10,10 +9,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 
 function TodoApp() {
-    const initialTodos = [{ id: 1, task: 'Pet a monkey', completed: false }];
-    const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(initialTodos);
-
-
     return (
         <Paper
             style={{
@@ -32,13 +27,8 @@ function TodoApp() {
             <Grid container justify='center' style={{ marginTop: '1rem' }}>
                 <Grid item xs={11} md={8} lg={4}>
                     <TodosProvider>
-                        <TodoForm addTodo={addTodo} />
-                        <TodoList
-                            todos={todos}
-                            removeTodo={removeTodo}
-                            toggleTodo={toggleTodo}
-                            editTodo={editTodo}
-                        />
+                        <TodoForm />
+                        <TodoList />
                     </TodosProvider>
                 </Grid>
             </Grid>
