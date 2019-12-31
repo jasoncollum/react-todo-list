2db487629fca6ee1907f8d3418ad8c178fe17f0c
Jason Collum <jasoncollum@gmail.com>
2019-12-31 17:09:51 -0600

    add useLocalStorageReducer hook, refactor todos.context to implement

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
-function useLocalStorageState(key, defaultValue) {
-    const [state, setState] = useState(() => {
-        let val;
-        try {
-            val = JSON.parse(window.localStorage.getItem(key) || String(defaultValue))
-        } catch (e) {
-            val = defaultValue
-        }
-        return val;
-    });
-    useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(state));
-    }, [state]);
-    return [state, setState];
-}
+// function useLocalStorageState(key, defaultValue) {
+//     const [state, setState] = useState(() => {
+//         let val;
+//         try {
+//             val = JSON.parse(window.localStorage.getItem(key) || String(defaultValue))
+//         } catch (e) {
+//             val = defaultValue
+//         }
+//         return val;
+//     });
+//     useEffect(() => {
+//         window.localStorage.setItem(key, JSON.stringify(state));
+//     }, [state]);
+//     return [state, setState];
+// }
 
-export default useLocalStorageState;
+// export default useLocalStorageState;
